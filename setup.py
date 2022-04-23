171a1427f90dea7cc896a5439dcbfa89c78e526e
Camille V Leonard <32983543+camillevleonard@users.noreply.github.com>
2022-04-23 10:35:57 -0400

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final products of our UVA capstone project Measuring the Impact of Open Source Software Using Network Analysis of Github Hosted Python Packages',
+    author='UVA MSDS OSS Capstone',
+    license='MIT',
+)
